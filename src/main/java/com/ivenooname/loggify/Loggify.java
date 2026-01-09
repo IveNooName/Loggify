3a8433c812fff6f867d9c8ad2e1ae900132ddfdd
IveNooName <fsc159548@stud.gibb.ch>
2026-01-09 14:02:21 +0100

    made some small changed, that the print can be better used

--- src/main/java/com/ivenooname/loggify/Loggify.java
+++ src/main/java/com/ivenooname/loggify/Loggify.java
@@ -1,19 +1,19 @@
 package com.ivenooname.loggify;
 
 public class Loggify {
-	public static void warn(String msg) {
+	public static void warn(Object msg) {
 		System.err.println("\u001B[33m[WARN]: " + msg + "\u001B[0m");
 	}
 
-	public static void error(String msg) {
+	public static void error(Object msg) {
 		System.err.println("\u001B[31m[ERROR]: " + msg + "\u001B[0m");
 	}
 
-	public static void info(String msg) {
+	public static void info(Object msg) {
 		System.out.println("\u001B[34m[INFO]: " + msg + "\u001B[0m");
 	}
 
-	public static void success(String msg) {
+	public static void success(Object msg) {
 		System.out.println("\u001B[32m[SUCCESS]: " + msg + "\u001B[0m");
 	}
 }
